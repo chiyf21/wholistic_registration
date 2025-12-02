07e43c5180cc9e232a528d88d40cae1e56b91d85
chiyf21 <118714807+chiyf21@users.noreply.github.com>
2025-12-02 21:39:04 +0800

    Create pipeline.py

---
+++ src/wholistic_registration/pipeline.py
@@ -0,0 +1,36 @@
+from core import main_function
+
+## Define data path and the normal config
+main_function.DefineParams(
+    configFile='./configs/config.toml',
+    inputFile='/home/cyf/wbi/Virginia/f338/221124_f338_ubi_gCaMP7f_bactin_mCherry_CAAX_7dpf002.nd2',
+    outputFile='./registrated_data/f338_registrated.zarr',
+    downsampleT=20, 
+    downsampleXY=4,  #choose downsample rate for registration(indenpendent to the dowsample below,just for accurate my debug)
+    downsampleZ=[4,5,6], #choose which planes to use
+    chunk_size=20,
+    mid_chunk_size=40,
+    k=50,
+    layer=1,
+    function='log10',
+    dual_channel=True,
+    verbose=False
+)
+
+## Do registration
+main_function.Registration(
+    './configs/config.toml'
+)
+
+## create downsample data
+main_function.create_downsample_dataset(
+    './configs/config.toml',
+    downsampleFilePath='./registrated_data/f338_registrated_downsample.zarr',
+    ds_XY=4,
+    ds_T=4
+)
+
+## reliable analysis
+# main_function.ReliableAnalysis(
+#     './configs/config.toml'
+# )
